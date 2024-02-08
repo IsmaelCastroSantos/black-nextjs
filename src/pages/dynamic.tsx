82528b5d0b57734c01f50603f284147f68cdcd31
Ismael Castro <IsmaelCastroSantos@outlook.com>
2024-02-08 16:13:46 -0300

    update page dynamic

--- src/pages/dynamic.tsx
+++ src/pages/dynamic.tsx
@@ -3,41 +3,40 @@ import { ReactNode, useEffect, useState } from "react";
 import { Col, Container, Row } from "reactstrap";
 
 interface ApiResponse {
-    name: string
-    timestamp: Date
+  name: string;
+  timestamp: Date;
 }
 
-export const getServerSideProps: GetServerSideProps = async() => {
-    const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverSideData: ApiResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_APIURL}/api/hello`
+  ).then((res) => res.json());
 
-    return {
-        props: {
-            serverSideData
-        }
-    }
-}
+  return {
+    props: {
+      serverSideData,
+    },
+  };
+};
 
 const Dynamic: NextPage = (props: {
-    children?: ReactNode
-    serverSideData?: ApiResponse
+  children?: ReactNode;
+  serverSideData?: ApiResponse;
 }) => {
-    const [clienteSideData, steClienteSideData] = useState<ApiResponse>()
+  const [clientSideData, setClientSideData] = useState<ApiResponse>();
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        const data = await fetch("/api/hello").then(res => res.json())
-        steClienteSideData(data)
-    }
-
+  const fetchData = async () => {
+    const data = await fetch("/api/hello").then((res) => res.json());
+    setClientSideData(data);
+  };
 
   return (
     <Container tag="main">
-      <h1 className="my-5">
-        Como funcionam as renderizações do Next.JS
-        </h1>
+      <h1 className="my-5">Como funcionam as renderizações do Next.JS</h1>
 
       <Row>
         <Col>
@@ -47,7 +46,7 @@ const Dynamic: NextPage = (props: {
 
         <Col>
           <h3>gerado no servidor:</h3>
-          <h2>{clienteSideData?.timestamp.toString()}</h2>
+          <h2>{clientSideData?.timestamp.toString()}</h2>
         </Col>
       </Row>
     </Container>
